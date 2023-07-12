8dab8d0eae0c4433d82cc8f7655ab4473d7faaab
Bill Armstrong <armsty9@gmail.com>
2023-07-12 16:59:11 +0300

    Server configured

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-import { Express } from "express";
+import express from 'express';
+// import { Middleware } from './middleware';
+// import { Routes } from './routes';
+
+const app = express();
+// Middleware(app);
+// Routes(app);
+
+app.listen(3000, () => {
+    console.log('🚀 🚀 🚀 🚀 Server running on port 3000🚀 🚀 🚀 🚀 ');
+    }
+);
