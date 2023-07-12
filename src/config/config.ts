26a356cdccf998bd31d091cda42d1447fd4b600f
Bill Armstrong <armsty9@gmail.com>
2023-07-12 16:52:19 +0300

    Added configuration file to hold the parameters

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+
+export const CONFIG = {
+    BYBIT_API_KEY: process.env.BYBIT_API_KEY || '',
+    BYBIT_API_SECRET: process.env.BYBIT_API_SECRET || '',
+    BYBIT_TESTNET:  'true',
+    MAIN_URL: process.env.MAIN_URL || '',
+}
